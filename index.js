b8ca2de44968dd9c92f3c001d710885b4246eca3
Bhaumik Chauhan <127184237+bhaumik0307@users.noreply.github.com>
2024-03-01 00:49:05 +0530

    commit

--- index.js
+++ index.js
@@ -1,73 +1,124 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-const app=express();
+const app = express();
 const port = 3000;
-var publicTask=[];
-var workTask=[];
-var date="";
+
+// var publicTask = [];
+var items = [];
+var workTask = [];
+var date = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(getDate);
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", { useNewUrlParser: true });
 
-function getDate(req, res, next){
+function getDate(req, res, next) {
     // Create a new Date object
-const currentDate = new Date();
+    const currentDate = new Date();
 
-// Get the day of the week as a number (0 = Sunday, 1 = Monday, etc.)
-const dayNumber = currentDate.getDay();
+    // Get the day of the week as a number (0 = Sunday, 1 = Monday, etc.)
+    const dayNumber = currentDate.getDay();
 
-// Create an array of day names
-const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    // Create an array of day names
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// Get the day name
-const dayName = dayNames[dayNumber];
+    // Get the day name
+    const dayName = dayNames[dayNumber];
 
-// Get the month as a number (0 = January, 1 = February, etc.)
-const monthNumber = currentDate.getMonth();
+    // Get the month as a number (0 = January, 1 = February, etc.)
+    const monthNumber = currentDate.getMonth();
 
-// Create an array of month names
-const monthNames = [
-  'January', 'February', 'March', 'April',
-  'May', 'June', 'July', 'August',
-  'September', 'October', 'November', 'December'
-];
+    // Create an array of month names
+    const monthNames = [
+        'January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August',
+        'September', 'October', 'November', 'December'
+    ];
 
-// Get the month name
-const monthName = monthNames[monthNumber];
+    // Get the month name
+    const monthName = monthNames[monthNumber];
 
-// Get the day of the month (1-31)
-const dayOfMonth = currentDate.getDate();
+    // Get the day of the month (1-31)
+    const dayOfMonth = currentDate.getDate();
 
-date= `${dayName}, ${monthName} ${dayOfMonth}`;
+    date = `${dayName}, ${monthName} ${dayOfMonth}`;
     next();
 }
-app.get("/",(req, res)=>{
-    res.render("index.ejs",{
-        publicElement: publicTask,
-        date: date,
+
+const itemSchema = {
+    name: String
+};
+
+const Item = new mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "Welcome to the To-Do-List"
+});
+const item2 = new Item({
+    name: "Click + to add a task"
+});
+const item3 = new Item({
+    name: "click to delete a task"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
+
+
+app.get("/", (req, res) => {
+    Item.find(function (err, result) {
+        if (result.length === 0) {
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("default items added successfully");
+                }
+            });
+            res.redirect('/');
+        }
+        else {
+            
+            res.render("index.ejs",{date: date, publicElement: result});
+        }
     });
+})
+app.post("/", (req, res) => {
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    })
+    item.save();
+    res.redirect('/');
+})
+app.post("/delete", (req, res) =>{
+    const itemId = req.body.check;
+    Item.findByIdAndRemove(itemId, function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("removed successfully");
+        }
+    })
+    res.redirect("/")
 })
 
-app.get("/work",(req, res)=>{
-    res.render("work.ejs",{
-        workElement: workTask,
-    });
+app.get("/work", (req, res) => {
+    res.render("work.ejs");
 })
-app.post("/",(req, res)=>{
-    publicTask.push(req.body.newItem);
-    res.render("index.ejs", {
-        publicElement: publicTask,
-        date: date,
-    })
-})
-app.post("/work",(req, res)=>{
+app.post("/work", (req, res) => {
     workTask.push(req.body.newItem);
-    res.render("work.ejs",{
+    res.render("work.ejs", {
         workElement: workTask,
     })
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 })
